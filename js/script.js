270c4785abf88c987a2c55dc1d03741ebf6fef32
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-09-30 18:58:42 +0200

    fix: change url image corrected

--- js/script.js
+++ js/script.js
@@ -9,27 +9,26 @@ let isLightOn = false;
 // console.log("mainHandler", mainHandler.classList);
 
 const turnOnLamp = function () {
-
-    lampImgHandler.src = "../img/yellow_lamp.png";
+    // da lezione 30/09: usare i due punti è ERRORE, funziona solo per via del liveserver.
+    // andando a sostituire la src nell'html il riferimento rimane e basta un punto
+    //lampImgHandler.src = "../img/yellow_lamp.png";
+    lampImgHandler.src = "./img/yellow_lamp.png";
     lampBtnHandler.innerHTML = `Ora clicca per spegnere`;
     mainHandler.classList.add("ligth-on");
 
 }
 
 const turnOffLamp = function () {
-    lampImgHandler.src = "../img/white_lamp.png";
+    lampImgHandler.src = "./img/white_lamp.png";
     lampBtnHandler.innerHTML = `Clicca per accendere`;
     mainHandler.classList.remove("ligth-on");
 }
 
 lampBtnHandler.addEventListener("click", function () {
-
     if (isLightOn) {
         turnOffLamp();
-
     } else {
         turnOnLamp();
-
     }
     isLightOn = !isLightOn;
     // console.log("isLightOn", isLightOn);
